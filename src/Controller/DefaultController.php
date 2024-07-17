57babd82f03e60a79dcef2a49eb3e49c350d7bf3
Michal Landsman <landsman@studioart.cz>
2024-07-17 11:15:53 +0200

    allow .env, add logger

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="demo")
      */
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+        $logger->info('Hello to betterstack!');
+
         return $this->render('index.html.twig');
     }
 }
